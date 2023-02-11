9ee926df0f141d8856c4b7a771a6a20c0e5587ce
Behemod <103079382+Behemod@users.noreply.github.com>
2023-02-11 22:06:37 +1000

    connected?

--- ImageAI/py/objD.py
+++ ImageAI/py/objD.py
@@ -1,13 +1,16 @@
-from imageai.Detection import ObjectDetection
-import os
+from imageai.Detection.Custom import CustomObjectDetection
+# import os
 
-execution_path = os.getcwd()
+# execution_path = os.getcwd()
 
-detector = ObjectDetection()
+detector = CustomObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath( os.path.join(execution_path , "ImageAI/yolov3.pt"))
+# detector.setModelPath( os.path.join(execution_path , "ImageAI/data-images/mytest/hololens-yolo/models/yolov3_hololens-yolo_last.pt"))
+detector.setModelPath("ImageAI/data-images/mytest/hololens-yolo/models/yolov3_hololens-yolo_last.pt")
+detector.setJsonPath("ImageAI/data-images/mytest/hololens-yolo/json/hololens-yolo_yolov3_detection_config.json")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "ImageAI/data-images/mytest/hud.jpg"), output_image_path=os.path.join(execution_path , "ImageAI/data-images/mytest/hud+.jpg"), minimum_percentage_probability=30)
+# detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "ImageAI/data-images/mytest/hololens-yolo/train/images/image (1).jpg"), output_image_path=os.path.join(execution_path , "ImageAI/data-images/mytest/holo+.jpg"), minimum_percentage_probability=30)
+detections = detector.detectObjectsFromImage(input_image="ImageAI/data-images/mytest/hololens-yolo/train/images/image (231).jpg", output_image_path="ImageAI/data-images/mytest/holo+.jpg", minimum_percentage_probability=30)
 
 for eachObject in detections:
     print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
