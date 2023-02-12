b105083b690a4c83fa83fe37013782dc1dc49b5e
Behemod <103079382+Behemod@users.noreply.github.com>
2023-02-12 16:44:00 +1000

    bet

--- ImageAI/py/objD.py
+++ ImageAI/py/objD.py
@@ -5,12 +5,12 @@ from imageai.Detection.Custom import CustomObjectDetection
 
 detector = CustomObjectDetection()
 detector.setModelTypeAsYOLOv3()
-# detector.setModelPath( os.path.join(execution_path , "ImageAI/data-images/mytest/hololens-yolo/models/yolov3_hololens-yolo_last.pt"))
-detector.setModelPath("ImageAI/data-images/mytest/hololens-yolo/models/yolov3_hololens-yolo_last.pt")
-detector.setJsonPath("ImageAI/data-images/mytest/hololens-yolo/json/hololens-yolo_yolov3_detection_config.json")
+# detector.setModelPath( os.path.join(execution_path , "ImageAI/mytest/hololens-yolo/models/yolov3_hololens-yolo_last.pt"))
+detector.setModelPath("ImageAI/mytest/hololens-yolo/models/yolov3_hololens-yolo_last.pt")
+detector.setJsonPath("ImageAI/mytest/hololens-yolo/json/hololens-yolo_yolov3_detection_config.json")
 detector.loadModel()
-# detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "ImageAI/data-images/mytest/hololens-yolo/train/images/image (1).jpg"), output_image_path=os.path.join(execution_path , "ImageAI/data-images/mytest/holo+.jpg"), minimum_percentage_probability=30)
-detections = detector.detectObjectsFromImage(input_image="ImageAI/data-images/mytest/hololens-yolo/train/images/image (231).jpg", output_image_path="ImageAI/data-images/mytest/holo+.jpg", minimum_percentage_probability=30)
+# detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "ImageAI/mytest/hololens-yolo/train/images/image (1).jpg"), output_image_path=os.path.join(execution_path , "ImageAI/mytest/holo+.jpg"), minimum_percentage_probability=30)
+detections = detector.detectObjectsFromImage(input_image="ImageAI/mytest/hololens-yolo/train/images/image (231).jpg", output_image_path="ImageAI/mytest/holo+.jpg", minimum_percentage_probability=30)
 
 for eachObject in detections:
     print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
--- ImageAI/py/train.py
+++ ImageAI/py/train.py
@@ -2,9 +2,10 @@ from imageai.Detection.Custom import DetectionModelTrainer
 
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory="ImageAI/data-images/mytest/hololens-yolo")
+trainer.setDataDirectory(data_directory="ImageAI/mytest/hololens-yolo")
 trainer.setTrainConfig(object_names_array=["hololens"], batch_size=32, num_experiments=200, train_from_pretrained_model="yolov3.pt")
 # In the above,when training for detecting multiple objects,
 #set object_names_array=["object1", "object2", "object3",..."objectz"]
 # Also batch_size=16 if 32 does not work
+# First try yolov3_hololens-yolo_mAP-0.57985_epoch-66.pt
 trainer.trainModel()
